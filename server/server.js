c2054371e67f195a40250685b07634137222691c
Kamnag R <rkamnag@gmail.com>
2018-09-22 17:31:37 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
 var config = require('config.json');
 
+//Routing
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
